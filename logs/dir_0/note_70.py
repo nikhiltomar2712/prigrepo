2ebc4f9d9120f2f2da704285c2e2d867f645c698
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-04-07 00:46:24 +0530

    docs: add log entry 70 on Prompt Hacking

---
+++ logs/dir_0/note_70.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #70
+# Topic: Prompt Hacking
+
+def log_entry_70():
+    # Jailbreaking techniques attempt to bypass safety filters of Large Language Models.
+    pass
