e4fd7e3b18d7c75a40b2b7404e6e9c21a59f4362
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-04-15 03:13:04 +0530

    docs: add log entry 350 on Prompt Hacking

---
+++ logs/dir_3/note_350.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #350
+# Topic: Prompt Hacking
+
+def log_entry_350():
+    # Adversarial prompts exploit the token representation to trigger unintended model behaviors.
+    pass
