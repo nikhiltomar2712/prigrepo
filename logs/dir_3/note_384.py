0a249ad567b2f7d685fab8f13ae599d2190aa9f9
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-04-16 02:49:44 +0530

    docs: add log entry 384 on Cybersecurity

---
+++ logs/dir_3/note_384.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #384
+# Topic: Cybersecurity
+
+def log_entry_384():
+    # SQL injection (SQLi) can be mitigated by using parameterized queries or prepared statements.
+    pass
