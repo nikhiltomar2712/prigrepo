2efac25f7ceec817c42c21a47bb2d74fda029f07
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-04-22 10:13:04 +0530

    docs: add log entry 602 on Prompt Hacking

---
+++ logs/dir_6/note_602.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #602
+# Topic: Prompt Hacking
+
+def log_entry_602():
+    # Adversarial prompts exploit the token representation to trigger unintended model behaviors.
+    pass
