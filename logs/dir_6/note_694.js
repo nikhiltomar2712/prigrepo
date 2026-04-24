3a69ed51d2a7bfe53cf9625d7a9de9bcab396981
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-04-25 02:06:24 +0530

    docs: add log entry 694 on Cybersecurity

---
+++ logs/dir_6/note_694.js
@@ -0,0 +1,7 @@
+// Log Entry #694
+// Topic: Cybersecurity
+
+function logEntry694() {
+    // SQL injection (SQLi) can be mitigated by using parameterized queries or prepared statements.
+    console.log('Cybersecurity: SQL injection (SQLi) can be mitigated by using parameterized queries or prepared statements.');
+}
