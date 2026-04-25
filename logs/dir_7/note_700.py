f1082bdea986c6e551d3318f661987c9a36fd8eb
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-04-25 06:16:24 +0530

    docs: add log entry 700 on Prompt Hacking

---
+++ logs/dir_7/note_700.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #700
+# Topic: Prompt Hacking
+
+def log_entry_700():
+    # Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.
+    pass
