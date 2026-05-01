5303c72d2e9871463ee4312cd8ea4de3257b21c5
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-02 01:28:04 +0530

    docs: add log entry 935 on Prompt Hacking

---
+++ logs/dir_9/note_935.js
@@ -0,0 +1,7 @@
+// Log Entry #935
+// Topic: Prompt Hacking
+
+function logEntry935() {
+    // Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.
+    console.log('Prompt Hacking: Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.');
+}
