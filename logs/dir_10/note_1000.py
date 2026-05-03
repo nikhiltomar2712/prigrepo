66c3a59695d42e6c1e185af51f21d7844c1fa40d
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-03 22:36:24 +0530

    docs: add log entry 1000 on Prompt Hacking

---
+++ logs/dir_10/note_1000.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1000
+# Topic: Prompt Hacking
+
+def log_entry_1000():
+    # Adversarial prompts exploit the token representation to trigger unintended model behaviors.
+    pass
