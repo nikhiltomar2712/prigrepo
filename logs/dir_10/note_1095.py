f6a29d7d68845e3b389b30385c22eef6f3847a4a
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-06 16:34:44 +0530

    docs: add log entry 1095 on Prompt Hacking

---
+++ logs/dir_10/note_1095.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1095
+# Topic: Prompt Hacking
+
+def log_entry_1095():
+    # Jailbreaking techniques attempt to bypass safety filters of Large Language Models.
+    pass
