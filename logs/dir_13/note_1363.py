06324db559723f8bd7104915809bb18235d1ba15
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-14 10:41:24 +0530

    docs: add log entry 1363 on Prompt Hacking

---
+++ logs/dir_13/note_1363.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1363
+# Topic: Prompt Hacking
+
+def log_entry_1363():
+    # Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.
+    pass
