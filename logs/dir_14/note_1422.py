61c7c1b0462194797f34a4d50f517331480a1c16
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-16 03:39:44 +0530

    docs: add log entry 1422 on Prompt Hacking

---
+++ logs/dir_14/note_1422.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1422
+# Topic: Prompt Hacking
+
+def log_entry_1422():
+    # LLM firewalls can inspect inputs and outputs to detect potential jailbreaks and PII leaks.
+    pass
