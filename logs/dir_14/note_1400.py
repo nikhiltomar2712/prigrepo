5035fa55977b7b14af8256dd852caf8e61925b29
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-15 12:23:04 +0530

    docs: add log entry 1400 on Prompt Hacking

---
+++ logs/dir_14/note_1400.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1400
+# Topic: Prompt Hacking
+
+def log_entry_1400():
+    # Jailbreaking techniques attempt to bypass safety filters of Large Language Models.
+    pass
