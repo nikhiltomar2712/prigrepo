eaa7d117815e5b788be5b47ab9a8d055efac20af
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-18 09:49:44 +0530

    docs: add log entry 1500 on Prompt Hacking

---
+++ logs/dir_15/note_1500.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1500
+# Topic: Prompt Hacking
+
+def log_entry_1500():
+    # Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.
+    pass
