e9245e19c96b7921faf9469522e7f126d3c5eacd
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-05-20 23:38:04 +0530

    docs: add log entry 1589 on Cybersecurity

---
+++ logs/dir_15/note_1589.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #1589
+# Topic: Cybersecurity
+
+def log_entry_1589():
+    # SQL injection (SQLi) can be mitigated by using parameterized queries or prepared statements.
+    pass
