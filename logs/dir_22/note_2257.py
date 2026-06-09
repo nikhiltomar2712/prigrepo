0f3315c7f82816efd99bb5e236a95d33a1e0dc6d
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-06-09 07:31:24 +0530

    docs: add log entry 2257 on Prompt Hacking

---
+++ logs/dir_22/note_2257.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #2257
+# Topic: Prompt Hacking
+
+def log_entry_2257():
+    # Adversarial prompts exploit the token representation to trigger unintended model behaviors.
+    pass
