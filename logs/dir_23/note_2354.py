0c0f9f0bc2f482d3a53ed1a783e7389b2933083a
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-06-12 02:53:04 +0530

    docs: add log entry 2354 on Prompt Hacking

---
+++ logs/dir_23/note_2354.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #2354
+# Topic: Prompt Hacking
+
+def log_entry_2354():
+    # Jailbreaking techniques attempt to bypass safety filters of Large Language Models.
+    pass
