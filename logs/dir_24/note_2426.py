2c486b604400aab27c2a21d238f834b83d4b3429
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-06-14 04:53:04 +0530

    docs: add log entry 2426 on Prompt Hacking

---
+++ logs/dir_24/note_2426.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Log Entry #2426
+# Topic: Prompt Hacking
+
+def log_entry_2426():
+    # Token smuggling uses obfuscation like Base64 or translation to hide adversarial prompts from detectors.
+    pass
