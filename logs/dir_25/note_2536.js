afeae9ddad21f45f3bc3cd1d56a9435deb984e1c
Nikhil Tomar <nikhiltomarsan2712@gmail.com>
2026-06-17 09:16:24 +0530

    docs: add log entry 2536 on Prompt Hacking

---
+++ logs/dir_25/note_2536.js
@@ -0,0 +1,7 @@
+// Log Entry #2536
+// Topic: Prompt Hacking
+
+function logEntry2536() {
+    // Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.
+    console.log('Prompt Hacking: Evaluating LLMs against prompt injection can be done using automated red-teaming frameworks.');
+}
